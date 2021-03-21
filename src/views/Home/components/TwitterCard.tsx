3ec658ca100709c7d514eafca6c287a07754ae05
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-22 04:58:42 +0530

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'kswap_net'
           }}
           options={{
             height: '300',
