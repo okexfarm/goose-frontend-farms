3d2d05b2ec69b7a73639bbb5f18c33a0a9b95b66
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-22 05:12:27 +0530

    Update DetailsSection.tsx

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -54,9 +54,9 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({
         <Text>{TranslateString(316, 'Stake')}:</Text>
         <StyledLinkExternal href={
           isTokenOnly ?
-            `https://exchange.goosedefi.com/#/swap/${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
+            `https://exchange.kswap.net/#/swap/${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
             :
-          `https://exchange.goosedefi.com/#/add/${liquidityUrlPathParts}`
+          `https://exchange.kswap.net/#/add/${liquidityUrlPathParts}`
         }>
           {lpLabel}
         </StyledLinkExternal>
